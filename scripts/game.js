f4b560b1a134b5f6f155b15d5feb7f0a90cc05ae
sarah2244-4 <douthwaite.sarah@yahoo.co.uk>
2023-10-01 21:56:22 +0100

    Added game object

---
+++ scripts/game.js
@@ -0,0 +1,20 @@
+// Game object
+
+let game = {
+	score: 0,
+	currentGame: [],
+	playerMoves: [],
+	choices: ["button1", "button2", "button3", "button4"],
+};
+
+// New game function
+
+// Add turn game - add random  turn to sequence
+
+// Light up the circle in the sequence
+
+// PLayer turn to check if player has clicked correct circle
+
+// Show score
+
+module.exports = { game };
--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -1,3 +1,29 @@
 /**
  * @jest-environment jsdom
  */
+const { game } = require("../game");
+
+beforeAll(() => {
+	let fs = require("fs");
+	let fileContents = fs.readFileSync("index.html", "utf-8");
+	document.open();
+	document.write(fileContents);
+	document.close();
+});
+
+describe("game object contains correct keys", () => {
+	test("score key exists", () => {
+		expect("score" in game).toBe(true);
+	});
+	test("currentGame key exists", () => {
+		expect("currentGame" in game).toBe(true);
+	});
+	test("player moves exists", () => {
+		expect("playerMoves" in game).toBe(true);
+	});
+	test("choices exists", () => {
+		expect("choices" in game).toBe(true);
+	});
+	test("choices contains correct ids", () =>
+		expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]));
+});
